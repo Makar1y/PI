14beeb1ef54ad9c0d0b0bdb577b268e5bfa59afe
Makar1y <makariyteam@gmail.com>
2025-09-25 20:38:25 +0300

    Refactor disk usage calculation to improve accuracy and readability

--- 3/main-b.sh
+++ 3/main-b.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 space=$((10*1024*1024*1024))
-used=$(du -s --block-size=1 | cut -f1)
+used=$(du -sb | cut -f1)
 left=$((space-used))
 percentage=$((100*used/left))
 sorted=$(du -b * | sort -nr)
